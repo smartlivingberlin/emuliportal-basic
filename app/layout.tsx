21c2462e88da0438713bef08eeaf3e1839299db3
smartlivingberlin <+@users.noreply.github.com>
2025-10-29 19:37:44 +0100

    chore: initial commit

--- app/layout.tsx
+++
@@ -1,9 +0,0 @@
-export const metadata = { title: "Emuliportal Basic", description: "Starter-Template" };
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="de">
-      <body>{children}</body>
-    </html>
-  );
-}
